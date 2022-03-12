03c0099bb7c2b9308e1f3b25c19852f7ad9227aa
monukumar98 <monu012as@gmail.com>
2022-03-12 18:07:33 +0530

    Linkedlist

--- Lec33/LinkedList.java
+++ Lec33/LinkedList.java
@@ -30,22 +30,73 @@ public class LinkedList {
 		}
 
 	}
+
 	public void addlast(int item) {
-		if(this.size==0) {
+		if (this.size == 0) {
 			addfirst(item);
-		}
-		else {
+		} else {
 			Node nn = new Node(item);
-			this.tail.next=nn;
-			this.tail=nn;
+			this.tail.next = nn;
+			this.tail = nn;
 			this.size++;
 		}
 	}
 
+	public void Addatindex(int k, int item) throws Exception {
+		if (k < 0) {
+			throw new Exception("Index glt h");
+		}
+
+		if (k == 0) {
+			addfirst(item);
+		} else if (k == this.size) {
+			addlast(item);
+		} else {
+			Node nn = new Node(item);
+			Node prev = GetNode(k - 1);
+			nn.next = prev.next;
+			prev.next = nn;
+			this.size++;
+
+		}
+
+	}
+
+	private Node GetNode(int k) throws Exception {
+		if (k < 0 || k >= this.size)
+			throw new Exception("Index glt h");
+		Node temp = this.head;
+		for (int i = 1; i <= k; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+	
+	
+	public int getfirst() throws Exception  {
+		if(this.size==0) {
+			throw new  Exception("Linked List is empty");
+		}
+		
+		return this.head.data;
+	}
+	public int getLast() throws Exception  {
+		if(this.size==0) {
+			throw new  Exception("Linked List is empty");
+		}
+		
+		return this.tail.data;
+	}
+	public int getatindex(int k) throws Exception {
+		return GetNode(k).data;
+	}
+	
+	
+
 	public void Display() {
 		Node temp = this.head;
 		while (temp != null) {
-			System.out.print(temp.data+"-->");
+			System.out.print(temp.data + "-->");
 			temp = temp.next;
 		}
 		System.out.println(".");
--- Lec33/LinkedList_Client.java
+++ Lec33/LinkedList_Client.java
@@ -2,7 +2,7 @@ package Lec33;
 
 public class LinkedList_Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		LinkedList ll = new LinkedList();
 		ll.addfirst(1);
@@ -12,6 +12,13 @@ public class LinkedList_Client {
 		ll.addlast(6);
 		ll.addlast(5);
 		ll.Display();
+		ll.Addatindex(2, 90);
+		ll.Display();
+		System.out.println(ll.getfirst());
+		System.out.println(ll.getLast());
+		System.out.println(ll.getatindex(3));
+		
+		
 		
 		
 
